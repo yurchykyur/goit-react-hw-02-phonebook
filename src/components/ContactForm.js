14598546360385ebc5a9ddbf683eb2edfc06327c
Yurchyk Yurii <124770215+yurchykyur@users.noreply.github.com>
2023-06-28 14:04:34 +0000

    add Step 4 + 5 and made styled/emotion

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+import {
+  FormWrapper,
+  ContactSubmitForm,
+  FormInputLabel,
+  FormInput,
+  FormSubmitBtn,
+} from './ContactForm.styled';
 
 const INITIAL_STATE_FORM = {
   name: '',
@@ -12,7 +21,6 @@ export default class ContactForm extends Component {
   handleInputChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
-    console.log(e.currentTarget.value);
   };
 
   reset = () => {
@@ -32,40 +40,42 @@ export default class ContactForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <h2>Name</h2>
-        <label htmlFor={this.nameInputId}>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleInputChange}
-            id={this.nameInputId}
-            value={this.state.name}
-          />
-        </label>
+      <FormWrapper>
+        <ContactSubmitForm onSubmit={this.handleFormSubmit}>
+          <FormInputLabel htmlFor={this.nameInputId}>
+            Name
+            <FormInput
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleInputChange}
+              id={this.nameInputId}
+              value={this.state.name}
+            />
+          </FormInputLabel>
 
-        <label htmlFor={this.numberInputId}>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleInputChange}
-            id={this.numberInputId}
-            value={this.state.number}
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
+          <FormInputLabel htmlFor={this.numberInputId}>
+            Phone number
+            <FormInput
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleInputChange}
+              id={this.numberInputId}
+              value={this.state.number}
+            />
+          </FormInputLabel>
+          <FormSubmitBtn type="submit">Add contact</FormSubmitBtn>
+        </ContactSubmitForm>
+      </FormWrapper>
     );
   }
 }
-// } ({ handleInputChange, handleFormSubmit }) {
 
-// }
-
-// onSubmit={}
+ContactForm.propTypes = {
+  formSubmitHandler: PropTypes.func.isRequired,
+};
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,26 @@
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+import { FilterWrapper, FilterInputLabel, FilterInput } from './Filter.styled';
 
 export default function Filter({ value, onChangeFilter }) {
   const inputId = nanoid();
   return (
-    <div>
-      <label htmlFor={inputId}>
-        <p>Find contacts by name:</p>
-        <input
+    <FilterWrapper>
+      <FilterInputLabel htmlFor={inputId}>
+        Find contacts by name:
+        <FilterInput
           type="text"
           value={value}
           id={inputId}
           onChange={e => onChangeFilter(e)}
         />
-      </label>
-    </div>
+      </FilterInputLabel>
+    </FilterWrapper>
   );
 }
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
